6eb91a533c8c2222c79515875c35a1bd53fb196c
FlorianxK <Florian.Koehler@jokonet.de>
2026-02-25 13:12:55 +0100

    day24 finished

--- Day24/day24.py
+++ Day24/day24.py
@@ -1,82 +1,116 @@
-from collections import deque
+from collections import defaultdict, deque
+import math
 from typing import *
 
 def dayTwentyFour():
+    tornados = defaultdict(set)
     arr = []
-    tornados = set()
-    torn = {'<':(0,-1), '>':(0,1), '^':(-1,0), 'v':(1,0)}
-    i = 0
     #read
-    with open("Day24/24.txt") as file:
-        for line in file:
-            arr.append( list(line.strip()) )
-            for j in range(len(line)):
-                if line[j] in "<>^v":
-                    tornados.add( (i,j,arr[i][j]) )
-            i += 1
+    with open("Day24/24_2.txt") as file:
+        for i,line in enumerate(file.read().splitlines()[1:-1]):
+            line = line[1:-1]
+            arr.append(line)
+            for j,c in enumerate(line):
+                if c in "<>^v":
+                    tornados[c].add( (i,j) )
 
     m,n = len(arr),len(arr[0])
-    start = (0,1)
-    end = (m-1,n-2)
-    arr[start[0]][start[1]] = 'S'
-    arr[end[0]][end[1]] = 'E'
-
-    def moveTornado():
-        newTorn = set()
-        for i,j,comp in tornados:
-            di,dj = torn[comp]
-            ni,nj = i+di,j+dj
-            if arr[ni][nj] == '#':
-                if comp == '<':
-                    nj = n-2
-                elif comp == '>':
-                    nj = 1
-                elif comp == '^':
-                    ni = m-2
-                elif comp == 'v':
-                    ni = 1
-            newTorn.add( (ni,nj,comp) )
-        return newTorn
-
-    def movePlayer(q):
-        # 2 possiblities: deque (i,j,time) wait or move on possible next -> do bfs and pop and add pop and all free to queue until one reaches E
-        newQ = deque([])
-        while q:
-            i,j,time = q.popleft()
-            #stand still if possible
-            if (i,j) not in tornados:
-                newQ.append( (i,j,time+1) )
-            #look if move possible
-            for di,dj in [(-1,0),(1,0),(0,-1),(0,1)]:
-                ni,nj = i+di,j+dj
-                if 0<=ni<m and 0 <=nj<n:
-                    #found end
-                    if arr[ni][nj] == 'E':
-                        return deque([(ni,nj,time+1)]),True
-
-                    if arr[ni][nj] == '#' or (ni,nj) in tornados:
-                        continue
-                    
-                    newQ.append( (ni,nj,time+1) )
-                
-        return newQ,False
-
+    start = (-1,0)
+    end = (m,n-1)
+    moves = {'^':(-1,0),'v':(1,0),'<':(0,-1),'>':(0,1)}
+    #(i,j,time)
     q = deque([(start[0],start[1],0)])
-    while True:
-        tornados = moveTornado()
-        q,found = movePlayer(q)
+    seen = set()
+    lcm = m*n//math.gcd(m,n)
 
-        if found:
-            break
-    return q[0][2]
+    while q:
+        i,j,time = q.popleft()
+
+        time += 1
+        for di,dj in [(-1,0),(1,0),(0,-1),(0,1),(0,0)]:
+            ni,nj = i+di,j+dj
+
+            if (ni,nj) == end:
+                return time
+
+            if (ni,nj) == start:
+                key = (ni,nj,time%lcm)
+                if key in seen:
+                    continue
+                seen.add(key)
+                q.append((ni,nj,time))
+            elif 0<=ni<m and 0<=nj<n:
+                for c,(ti,tj) in moves.items():
+                    newTi = (ni - ti*time) % m
+                    newTj = (nj - tj*time) % n
+                    if (newTi,newTj) in tornados[c]:
+                        break
+                else:
+                    key = (ni,nj,time%lcm)
+                    if key in seen:
+                        continue
+                    seen.add(key)
+                    q.append((ni,nj,time))
 
 def dayTwentyFour2():
-    pass
+    tornados = defaultdict(set)
+    arr = []
+    #read
+    with open("Day24/24_2.txt") as file:
+        for i,line in enumerate(file.read().splitlines()[1:-1]):
+            line = line[1:-1]
+            arr.append(line)
+            for j,c in enumerate(line):
+                if c in "<>^v":
+                    tornados[c].add( (i,j) )
+
+    m,n = len(arr),len(arr[0])
+    start = (-1,0)
+    ends = [(m,n-1),(-1,0)]
+    moves = {'^':(-1,0),'v':(1,0),'<':(0,-1),'>':(0,1)}
+    #(i,j,time,stage)
+    q = deque([(start[0],start[1],0,0)])
+    seen = set()
+    lcm = m*n//math.gcd(m,n)
+
+    while q:
+        i,j,time,stage = q.popleft()
+
+        time += 1
+        for di,dj in [(-1,0),(1,0),(0,-1),(0,1),(0,0)]:
+            ni,nj = i+di,j+dj
+            nstage = stage
+            if (ni,nj) == ends[stage%2]:
+                if stage == 2:
+                    return time
+                nstage += 1
+
+            fail = False
+            if (ni,nj) in ends:
+                key = (ni,nj,time%lcm,nstage)
+                if key in seen:
+                    continue
+                seen.add(key)
+                q.append((ni,nj,time,nstage))
+            elif 0<=ni<m and 0<=nj<n:
+                for c,(ti,tj) in moves.items():
+                    newTi = (ni - ti*time) % m
+                    newTj = (nj - tj*time) % n
+                    if (newTi,newTj) in tornados[c]:
+                        fail = True
+                        break
+
+                if not fail:
+                    key = (ni,nj,time%lcm,nstage)
+                    if key in seen:
+                        continue
+                    seen.add(key)
+                    q.append((ni,nj,time,nstage))
 
 def main():
     print("Hallo")
     print(dayTwentyFour(), "ist die Lösung von Teil 1")
-    #print(dayTwentyFour2(), "ist die Lösung von Teil 2")
+    print(dayTwentyFour2(), "ist die Lösung von Teil 2")
      
 if __name__=="__main__":
     main()
